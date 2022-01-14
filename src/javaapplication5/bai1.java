8a416dd5ecf466770ae2b365ec0c21a376e3a5cb
Nothingtosay <79694084+nothings0@users.noreply.github.com>
2022-01-14 15:20:05 +0700

    update

--- src/javaapplication5/bai1.java
+++ src/javaapplication5/bai1.java
@@ -2,6 +2,6 @@ package javaapplication5;
 
 public class bai1 {
     public static void main(String[] args) {
-        System.out.println("Hello các bạn");
+        System.out.println("Wellcom to Java!");
     }
 }
